cad30b96c1e8455a06299f53ce2d9d6c9024c29f
Moise6798 <otzenberger.nicolas@gmail.com>
2023-11-16 11:52:15 +0100

    so_long

--- So_long/so_long.h
+++ So_long/so_long.h
@@ -6,17 +6,26 @@
 /*   By: niotzenb <niotzenb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 09:54:27 by niotzenb          #+#    #+#             */
-/*   Updated: 2023/11/14 10:48:35 by niotzenb         ###   ########.fr       */
+/*   Updated: 2023/11/16 11:33:49 by niotzenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include <libc.h>
 # include <unistd.h>
 # include <math.h>
-# include <mlx.h>
 # include <stdlib.h>
+# include "minilibx-linux/mlx.h"
+# include <X11/X.h>
+# include <X11/keysym.h>
+# include <stdio.h>
+
+typedef struct s_data
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	void	*textures[5];
+}	t_data;
 
 #endif
--- So_long/so_long_main.c
+++ So_long/so_long_main.c
@@ -6,13 +6,47 @@
 /*   By: niotzenb <niotzenb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 09:52:59 by niotzenb          #+#    #+#             */
-/*   Updated: 2023/11/14 11:52:14 by niotzenb         ###   ########.fr       */
+/*   Updated: 2023/11/16 11:47:26 by niotzenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	main(int argc, char *argv[])
+int	handle_no_event(void *data)
 {
-	
+	return (0);
 }
+
+int	handle_keypress(int keysym, t_data *data)
+{
+	if (keysym == XK_Escape)
+		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+	printf("Keypress: %d\n", keysym);
+	return (0);
+}
+
+int	handle_keyrelease(int keysym, void *data)
+{
+	printf("keyrelease: %d\n", keysym);
+	return (0);
+}
+
+int	main(void)
+{
+	t_data	data;
+
+	data.mlx_ptr = mlx_init();
+	if (data.mlx_ptr == NULL)
+		return (1);
+	data.win_ptr = mlx_new_window(data.mlx_ptr, 600, 400, "hi :)");
+	if (data.win_ptr == NULL)
+		return (free(data.mlx_ptr), 1);
+	mlx_loop_hook(data.mlx_ptr, &handle_no_event, &data);
+	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &data);
+	mlx_hook(data.win_ptr, KeyRelease, KeyPressMask, &handle_keyrelease, &data);
+	mlx_loop(data.mlx_ptr);
+	mlx_destroy_display(data.mlx_ptr);
+	free(data.mlx_ptr);
+	return (0);
+}
+
