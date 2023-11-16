0ab10a5b5812b1d74172c2b72ff7ddb86cbfe0c2
Moise6798 <otzenberger.nicolas@gmail.com>
2023-11-16 09:15:52 +0100

    so_long

---
+++ So_long/so_long.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: niotzenb <niotzenb@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/14 09:54:27 by niotzenb          #+#    #+#             */
+/*   Updated: 2023/11/14 10:48:35 by niotzenb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <libc.h>
+# include <unistd.h>
+# include <math.h>
+# include <mlx.h>
+# include <stdlib.h>
+
+#endif
