9eb9885ec300d090986d4c382c91c1f1a9fcfbde
Moise6798 <otzenberger.nicolas@gmail.com>
2023-11-20 10:16:01 +0100

    ft_printf

--- So_long/so_long.h
+++ So_long/so_long.h
@@ -6,33 +6,41 @@
 /*   By: niotzenb <niotzenb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 09:54:27 by niotzenb          #+#    #+#             */
-/*   Updated: 2023/11/16 12:40:41 by niotzenb         ###   ########.fr       */
+/*   Updated: 2023/11/20 10:10:26 by niotzenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include <unistd.h>
-# include <math.h>
+# include <string.h>
 # include <stdlib.h>
 # include "minilibx-linux/mlx.h"
 # include <X11/X.h>
 # include <X11/keysym.h>
 # include <stdio.h>
 
-# define WINDOW_WIDTH 600
-# define WINDOW_HEIGHT 300
+# define WINDOW_WIDTH 1200
+# define WINDOW_HEIGHT 600
 
 # define RED_PIXEL 0xFF0000
 # define GREEN_PIXEL 0xFF00
 # define WHITE_PIXEL 0xFFFFFF
 
+typedef struct s_img
+{
+	void	*mlx_img;
+	char	*addr;
+	int		bpp;
+	int		line_len;
+	int		endian;
+}	t_img;
+
 typedef struct s_data
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
-	void	*textures[5];
+	t_img	img;
 }	t_data;
 
 typedef struct s_rect
@@ -43,5 +51,7 @@ typedef struct s_rect
 	int	height;
 	int	color;
 }	t_rect;
+
+
 
 #endif
--- So_long/so_long_main.c
+++ So_long/so_long_main.c
@@ -6,7 +6,7 @@
 /*   By: niotzenb <niotzenb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 09:52:59 by niotzenb          #+#    #+#             */
-/*   Updated: 2023/11/16 12:43:36 by niotzenb         ###   ########.fr       */
+/*   Updated: 2023/11/20 10:07:02 by niotzenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,50 +26,68 @@ int	handle_keypress(int keysym, t_data *data)
 	}
 	return (0);
 }
+void	img_pix_put(t_img *img, int x, int y, int color)
+{
+	char	*pixel;
+	int		i;
 
-void	render_background(t_data *data, int color)
+	i = img->bpp - 8;
+	pixel = img->addr + (y * img->line_len + x *(img->bpp / 8));
+	while (i >= 0)
+	{
+		if (img->endian != 0)
+			*pixel++ = (color >> i) & 0xFF;
+		else
+			*pixel++ = (color >> (img->bpp - 8 - i)) & 0xFF;
+		i -= 8;
+	}
+}
+
+void	render_background(t_img *img, int color)
 {
 	int	i;
 	int	j;
 
-	if (data->win_ptr == NULL)
-		return ;
 	i = 0;
 	while (i < WINDOW_HEIGHT)
 	{
 		j = 0;
 		while (j < WINDOW_WIDTH)
-			mlx_pixel_put(data->mlx_ptr, data->win_ptr, j++, i, color);
+			img_pix_put(img, j++, i, color);
 		++i;
 	}
 }
 
-int	render_rectangle(t_data *data, t_rect rect)
+int	render_rectangle(t_img *img, t_rect rect)
 {
 	int	i;
 	int	j;
 
-	if (data->win_ptr == NULL)
-		return (1);
 	i = rect.y;
 	while (i < rect.y + rect.height)
 	{
 		j = rect.x;
 		while (j < rect.x + rect.width)
-			mlx_pixel_put(data->mlx_ptr, data->win_ptr, j++, i, rect.color);
-		i++;
+			img_pix_put(img, j++, i, rect.color);
+		++i;
 	}
 	return (0);
 }
 
 int	render(t_data *data)
 {
-	render_background(data, WHITE_PIXEL);
-	render_rectangle(data, (t_rect){WINDOW_WIDTH - 100, WINDOW_HEIGHT - 100,
-		100, 100, GREEN_PIXEL});
-	render_rectangle(data, (t_rect){0, 0, 100, 100, RED_PIXEL});
+	if (data->win_ptr == NULL)
+		return (1);
+	render_background(&data->img, WHITE_PIXEL);
+	render_rectangle(&data->img, (t_rect){WINDOW_WIDTH - 100,
+		WINDOW_HEIGHT - 100, 100, 100, GREEN_PIXEL});
+	render_rectangle(&data->img, (t_rect){0, 0, 100, 100, RED_PIXEL});
+	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.mlx_img,
+		0, 0);
 	return (0);
 }
+
+
 
 int	main(void)
 {
@@ -85,9 +103,13 @@ int	main(void)
 		free(data.win_ptr);
 		return (1);
 	}
+	data.img.mlx_img = mlx_new_image(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
+	data.img.addr = mlx_get_data_addr(data.img.mlx_img, &data.img.bpp,
+			&data.img.line_len, &data.img.endian);
 	mlx_loop_hook(data.mlx_ptr, &render, &data);
 	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, &handle_keypress, &data);
 	mlx_loop(data.mlx_ptr);
+	mlx_destroy_image(data.mlx_ptr, data.img.mlx_img);
 	mlx_destroy_display(data.mlx_ptr);
 	free(data.mlx_ptr);
 	return (0);
